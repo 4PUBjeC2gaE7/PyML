bffea56369c49e6907560f542ebb1a0be32f827d
Hector <08110ector.0x4Dalagon@gmail.com>
2022-10-03 21:04:23 -0600

    added "get_prior" subroutine

--- Naive Bayes/bayes1.py
+++ Naive Bayes/bayes1.py
@@ -12,6 +12,19 @@ def get_label_indeces(labels):
         label_indices[label].append(index)
     return label_indices
 
+def get_prior(label_indices):
+    '''
+    Compute prior based on training samples
+    @param label_indices: gropued sample indices by class
+    @return: dictionary, with class label as key, corresponding prior as
+             the value
+    '''
+    prior = {label: len(indices) for label, indices in label_indices.items()}
+    total_count = sum(prior.values())
+    for label in prior:
+        prior[label] /= total_count
+    return prior
+
 if __name__ == '__main__':
     # X training array
     xTrain = np.array([
@@ -28,3 +41,6 @@ if __name__ == '__main__':
 
     labelIdx = get_label_indeces(yTrain)
     print(labelIdx)
+
+    prior = get_prior(labelIdx)
+    print(f'Prior: {prior}')
